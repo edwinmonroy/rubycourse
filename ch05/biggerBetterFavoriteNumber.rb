8bc523a77f83f8dadd0d73707aa9a86ba93644c6
Edwin <emonroy@berkeley.edu>
2017-12-22 01:13:12 -0800

    add ch05

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,12 @@
+puts ''
+puts 'hello random stranger' + '! '
+puts ''
+puts 'This is the NS' + '...' + 'I mean your computer' + '!'
+puts ''
+puts 'We' + '...' + 'I was wondering, what is your favorite big number?'
+number = gets.chomp
+puts ''
+puts 'You know what is better than that?'
+puts number.to_i + 1
+puts ''
+puts 'Now that is a bigger and better favorite number!'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,16 @@
+puts 'Hi there, what is your name?'
+first = gets.chomp
+puts ''
+puts 'Nice to meet you ' + first + '!'
+puts ''
+puts 'What is your middle name' + '?'
+middle = gets.chomp
+puts ''
+puts middle + '?' + ' What a weird middle name...'
+puts ''
+puts 'Anyways, what is your last name' + '?'
+last = gets.chomp
+puts ''
+puts 'Really' + '? ' + last + '?'
+puts ''
+puts 'Well, it is nice to meet you ' + first + ' ' + middle + ' ' + last + '!'

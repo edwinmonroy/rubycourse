02347374adce234744922f5f0dfc31564a2dc056
Edwin <emonroy@berkeley.edu>
2017-12-29 21:18:22 -0800

    message

---
+++ ch06/angryBoss.rb
@@ -0,0 +1,16 @@
+puts
+puts
+puts 'Whatchu up to my lovely favorite boss?'
+puts
+puts
+puts 'Stop being a kiss ass and tell me what you want.'
+puts
+puts
+puts 'uh...well...'
+request = gets.chomp
+puts
+puts
+puts '...you wasted my time for that?'
+puts
+puts
+puts (request + ' is the stupidest request I have ever heard!').upcase
